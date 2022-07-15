b0435e56a059b474893fc5fb40a32a6fe4df0d47
danny-englander <dannyenglander311@gmail.com>
2022-07-15 15:00:00 -0400

    Starting question data

--- src/data/gameData.js
+++ src/data/gameData.js
@@ -1,15 +1,16 @@
-export const questions = [
+const questionData = [
   {
-    question: "my question",
+    question: "What person is attributed with this quote? 'I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin, but by the content of their character.'",
     incorrectAnswers: [
-      "",
-      "",
-      "",
+      "Harriet Tubman",
+      "Rosa Parks",
+      "Malcom X",
     ],
     correctAnswers: [
-      "",
-      "",
+      "Martin Luther King",
     ],
     questionImage: ""
   },
 ];
+
+export default questionData;
