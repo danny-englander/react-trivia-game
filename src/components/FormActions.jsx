6e5c06c3b1648da92610bd46f6dd70b4b734bee7
Danny Englander <dannyenglander311@gmail.com>
2022-08-03 05:46:28 -0400

    Working on formactions

--- src/components/FormActions.jsx
+++ src/components/FormActions.jsx
@@ -1,45 +1,67 @@
 import React, { useContext } from 'react'
 import { GameContext } from '../contexts/GameContext'
-import styleFormActions from '../assets/scss/components/form-actions.module.scss'
+import styleFormActions from '../assets/scss/components/formactions.module.scss'
 
 const FormActions = ({ onLockClick, onNextClick, formValidationMessage }) => {
   const [gameState] = useContext(GameContext)
   return (
-    <div className={styleFormActions.wrap}>
-      {/* Enable disable buttons below with conditions and add accessibility. */}
-      {/*
-       *** Lock answer button.
-       */}
-      {/* Form validation  */}
+    <div className={styleFormActions.btnItems}>
+      {/* Form validation message.  */}
       {formValidationMessage && <p className="">{formValidationMessage}</p>}
 
-      <button
-        disabled={gameState.answerLocked === true}
-        aria-disabled={gameState.answerLocked === true}
-        onClick={onLockClick}
-        className={`${styleFormActions.btn} ${styleFormActions.btnPrimary} ${
-          gameState.answerLocked === true ? 'foo' : 'bar'
-        }`}
-      >
-        Lock in Answer!
-      </button>
+      {/* Enable disable buttons below with conditions and add accessibility. */}
 
-      {/*
-       *** Next button.
-       */}
-      <button
-        disabled={gameState.answerLocked === false}
-        aria-disabled={gameState.answerLocked === false}
-        className={`${styleFormActions.btn} ${styleFormActions.btnSecondary} ${
-          gameState.answerLocked === true ? 'foo' : 'bar'
-        }`}
-        // aria-describedby={}
-        onClick={onNextClick}
+      <div
+        className={`${styleFormActions.btnItem} ${styleFormActions.btnItemLock}`}
       >
-        {gameState.currentQuestionIndex === gameState.questions.length - 1
-          ? 'Finish the game!'
-          : 'Next question'}
-      </button>
+        {/*
+         *** Lock answer button.
+         */}
+        <button
+          disabled={gameState.answerLocked === true}
+          aria-disabled={gameState.answerLocked === true}
+          onClick={onLockClick}
+          className={`${styleFormActions.btn} ${styleFormActions.btnPrimary} ${
+            gameState.answerLocked === true ? 'foo' : 'bar'
+          }`}
+        >
+          Lock in Answer!
+        </button>
+      </div>
+
+      <div
+        className={`${styleFormActions.btnItem} ${styleFormActions.btnItemNext}`}
+      >
+        {/*
+         *** Next button.
+         */}
+        <button
+          // disabled={gameState.answerLocked === false}
+          aria-disabled={gameState.answerLocked === false}
+          className={`${styleFormActions.btn} ${
+            styleFormActions.btnSecondary
+          } ${
+            gameState.answerLocked === true
+              ? `${styleFormActions.btnEnabled}`
+              : `${styleFormActions.btnDisabled}`
+          }`}
+          aria-describedby={
+            gameState.answerLocked === false && 'next-button-tip'
+          }
+          onClick={onNextClick}
+        >
+          {gameState.currentQuestionIndex === gameState.questions.length - 1
+            ? 'Finish the game!'
+            : 'Next question'}
+        </button>
+
+        <div className={styleFormActions.tipWrap}>
+          <p id="next-button-tip" className={styleFormActions.tip}>
+            This button is disabled, you first need to choose an answer and
+            select "Lock in Answer" to proceed in the game.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
