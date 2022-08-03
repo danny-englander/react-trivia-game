5f1828590b6434671ea75eba798eefec04a89b71
danny-englander <dannyenglander311@gmail.com>
2022-08-03 10:35:07 -0400

    colors update / put strictmode back in

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
