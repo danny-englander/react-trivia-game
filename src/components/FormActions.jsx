d905e3114d952061bc8318850a015a7d8e1370d0
danny-englander <dannyenglander311@gmail.com>
2022-08-04 11:26:11 -0400

    Refactor to only show one game button, lock or next

--- src/components/FormActions.jsx
+++ src/components/FormActions.jsx
@@ -23,66 +23,26 @@ const FormActions = ({ onLockClick, onNextClick, formValidationMessage }) => {
 
       {/* Enable disable buttons below with conditions and add accessibility. */}
       <div className={styleFormActions.btnItems}>
-        {/*
-         *** Lock answer button.
-         */}
         <div
           className={`${styleFormActions.btnItem} ${styleFormActions.btnItemLock}`}
         >
-          <button
-            disabled={gameState.answerLocked === true}
-            aria-disabled={gameState.answerLocked === true}
-            onClick={onLockClick}
-            className={`${styleFormActions.btn} ${
-              styleFormActions.btnPrimary
-            } ${gameState.answerLocked === true ? 'locked' : 'unlocked'}
-            `}
-          >
-            Lock in Answer!
-          </button>
-        </div>
-
-        {/*----------------------------------------------------*/}
-
-        {/*
-         *** Next button.
-         */}
-        <div
-          className={`${styleFormActions.btnItem} ${
-            gameState.answerLocked === false
-              ? `${styleFormActions.toolTipActive}`
-              : ''
-          }`}
-        >
-          <button
-            aria-disabled={gameState.answerLocked === false}
-            className={`${styleFormActions.btn} ${
-              styleFormActions.btnSecondary
-            } ${
-              gameState.answerLocked === true
-                ? `${styleFormActions.btnEnabled}`
-                : `${styleFormActions.btnDisabled}`
-            }`}
-            aria-describedby={
-              gameState.answerLocked === false && 'disabledReason'
-            }
-            onClick={onNextClick}
-          >
-            {/* Contextual button text depending on where you are in the game. */}
-            {gameState.currentQuestionIndex === gameState.questions.length - 1
-              ? 'Finish the game!'
-              : 'Next question'}
-          </button>
-
-          {/*
-           *** Tooltip for aria-describedby.
-           */}
-          <div id="disabledReason" className={styleFormActions.tipWrap}>
-            <p className={styleFormActions.tip}>
-              This button is disabled, you first need to choose an answer and
-              select "Lock in Answer" to proceed in the game.
-            </p>
-          </div>
+          {gameState.answerLocked === false ? (
+            <button
+              className={`${styleFormActions.btn} ${styleFormActions.btnPrimary}`}
+              onClick={onLockClick}
+            >
+              Lock in Answer!
+            </button>
+          ) : (
+            <button
+              className={`${styleFormActions.btn} ${styleFormActions.btnSecondary}`}
+              onClick={onNextClick}
+            >
+              {gameState.currentQuestionIndex === gameState.questions.length - 1
+                ? 'Finish the game!'
+                : 'Next question'}
+            </button>
+          )}
         </div>
       </div>
     </>
